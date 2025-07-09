45f3fc10d8b7514fd166af2341819723150513ff
ChKik <xariskikidis5@gmail.com>
2025-07-09 11:53:01 +0300

    Created scripts file and images for preprocessing

---
+++ scripts/image_preprocessing.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os, os.path
+
+resize_width=224
+resize_height=224
+
+#checks  to current directory gia number of files.
+for x in range(1,len(os.listdir('/home/haris/thesis_project/image_preprocessing'))):
+    fd=Image.open(f"{x}.jpg","r")
+    fd=fd.resize((resize_width,resize_height),Image.LANCZOS)
+    print("Resized image")
+    print(fd)
+    
+    base_name=os.path.splitext(fd)[0] #pairnei to onoma toy file mono.
+    new_img= fd.save('/home/haris/thesis_project/image_preprocessing',f"{base_name}_resized.jpg",format='JPEG')
+
